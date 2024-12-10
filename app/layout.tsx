a6d509d9ec2d72bd3282b5395fbb968acfc1c250
Htet Rann <166049789+HtetRann@users.noreply.github.com>
2024-12-10 17:00:01 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { ThemeProvider } from '@/components/providers/ThemeProvider';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'TraversyPress',
+  title: 'adminDashboard-sample',
   description: 'Admin dashboard',
 };
 
